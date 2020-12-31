217f1d34c4a246a9fa4e60448190da0f90c7c0b1
Alexandre <criabio@hotmail.com>
2020-12-31 16:00:11 -0300

    first commit, project configs

---
+++ app.js
@@ -0,0 +1,26 @@
+/* Carregando módulos */
+const express = require('express');
+const handlebars = require('express-handlebars');
+const bodyParser = require('body-parser');
+const app = express();
+// const mongoose = require('mongoose');
+
+/* Configs */
+/* Body parser */
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+/* Handlebars */
+app.engine('handlebars', handlebars({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+/* Mongoose */
+// breve
+
+/* Rotas */
+
+/* Outros */
+const PORT = 8081;
+app.listen(PORT, () => {
+  console.log('servidor rodando!');
+});
