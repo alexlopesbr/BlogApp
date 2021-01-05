dec17fc610ae39afd26f6e2c976d68f3c129e978
Alexandre <criabio@hotmail.com>
2021-01-05 08:08:54 -0300

    form validation added

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(flash());
 
 /* Middleware */
 app.use((req, res, next) => {
-  res.locals.success = req.flash('success_msg');
+  res.locals.success_msg = req.flash('success_msg');
   res.locals.error_msg = req.flash('error_msg');
   next();
 });
